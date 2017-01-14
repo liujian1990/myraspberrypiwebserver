44a81e0de0fffc82ebe8bca49d043381c1c89493
liujiande1990 <liujiandemail@163.com>
2017-01-14 16:39:16 +0000

    fix some bug.

--- test_flask.py
+++ test_flask.py
@@ -1,10 +1,33 @@
 #test for flask
-from flask import Flask
+from flask import Flask,url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def content():
+@app.route('/')
+def context():
     return "This liujian's website."
-if __name__ == "main":
-    app.run(host="0.0.0.0", port=80,debug=True)
+
+@app.route('/index')
+def index():
+    return "index"
+
+@app.route('/login')
+def login(): pass
+
+@app.route('/user/<username>')
+def profile(username): pass
+
+with app.test_request_context():
+    print url_for('index')
+    print url_for('login')
+    print url_for('login', next='/')
+    print url_for('profile', username='lj')
+    
+if __name__ == "__main__":
+    try:
+        print "Start webserver."
+        #app.run()
+    	app.run(host="0.0.0.0", port=80,debug=True)
+        print "A web Server end."  
+    except Exception,e:
+        print Exception,":",e
